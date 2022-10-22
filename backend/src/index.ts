d36ab79569d9a1be101968e534948984bbb771e3
Joonas Niemi <joonas.niemi@aalto.fi>
2022-10-22 17:28:49 +0300

    format template project with Prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const PORT = process.env.PORT || 3030;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server running on http://localhost:${PORT}`)
+);
