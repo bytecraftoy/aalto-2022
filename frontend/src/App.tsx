8658fd0641782bc46d2142fabad0b40c4e10b5c4
Joonas Niemi <joonas.niemi@aalto.fi>
2022-10-29 17:40:08 +0300

    convert template app css to tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <header className="App-header h-screen bg-react text-white text-2xl justify-center items-center flex flex-col">
+        <div className="animate-spin">
+          <img src={logo} className="App-logo h-40" alt="logo" />
+        </div>
+        <p className="">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-cyan-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
