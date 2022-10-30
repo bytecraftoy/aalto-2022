0196918a1c588c8a77ee8b5e13fde2b485d2b3f0
Tommi Korpelainen <tommi.korpelainen@aalto.fi>
2022-10-30 17:40:15 +0200

    Dummy test now forces server to close before exiting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,16 @@ import express from "express";
 
 const PORT = process.env.PORT || 3030;
 
-const testNumber = 42;
-
 const app = express();
+
+const magicNumber = 42;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`server running on http://localhost:${PORT}`)
 );
 
-export { testNumber };
+export { server, magicNumber };
--- backend/src/tests/index.test.ts
+++ backend/src/tests/index.test.ts
@@ -1,5 +1,6 @@
-import { testNumber } from "..";
+import { magicNumber, server } from "../index";
 
 test("The magic number should be 42", () => {
-  expect(testNumber).toBe(42);
+  expect(magicNumber).toBe(42);
+  server.close();
 });
