3c52d8bb9dab23fc87586c745c6c6bf3032aa426
Ilari Tulkki <ilari.tulkki@aalto.fi>
2022-10-31 13:55:17 +0200

    -

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
-import http from 'http';
-import app from './app';
+import http from "http";
+import app from "./app";
 
 const PORT = process.env.PORT || 3030;
 
 const server = http.createServer(app);
 
-if(process.env.NODE_ENV === 'test')
-    server.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
+if (process.env.NODE_ENV === "test")
+  server.listen(PORT, () =>
+    console.log(`server running on http://localhost:${PORT}`)
+  );
 
 export default server;
