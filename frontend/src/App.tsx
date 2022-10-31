49b1765b661ba763f86d76358aa540513dd1c148
unknown <korpelainentommi@gmail.com>
2022-10-31 14:59:31 +0200

    Adding panels

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,25 @@
 import React from "react";
-import { CodeBracketIcon } from "@heroicons/react/24/outline";
-import { TailwindTestModal } from "./components/TailwindTestModal";
+//import { CodeBracketIcon } from "@heroicons/react/24/outline";
+import { ContentPanel } from "./components/ContentPanel";
+import { PromptCategoryBox } from "./components/PromptCategoryBox";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header min-h-screen bg-react text-white text-2xl justify-center items-center flex flex-col">
-        <div className="animate-spin">
-          <CodeBracketIcon className="App-logo h-40" />
-        </div>
-        <p className="">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-cyan-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="min-w-full py-2">
-          <TailwindTestModal />
+    <div className="App bg-react min-h-screen justify-start items-center flex flex-col">
+      <header className="App-header text-white text-2xl py-10">
+        <div className="">
+          <h1 className="App-logo font-medium text-4xl">
+            Aalto-2022 AI assisted game content creator
+          </h1>
         </div>
       </header>
+      <div className="min-w-full py-2">
+        <ContentPanel>
+          <div className="flex flex-row justify-center items-center">
+            <PromptCategoryBox />
+          </div>
+        </ContentPanel>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/PromptIOBox.tsx
@@ -0,0 +1,3 @@
+export const PromptIOBox = () => {
+  return <></>;
+};
