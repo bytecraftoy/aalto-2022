1caefd37e493e635c1c7ca7e041b28b3393a23ce
unknown <korpelainentommi@gmail.com>
2022-10-31 14:04:54 +0200

    Formatted everything >:)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world");
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world');
 });
 
 export default app;
