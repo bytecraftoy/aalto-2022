742a83e435f2277c74552323589d04d43f608953
Ilari Tulkki <ilari.tulkki@aalto.fi>
2022-10-31 14:02:00 +0200

    fixed the wrong camparison operator

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3030;
 
 const server = http.createServer(app);
 
-if (process.env.NODE_ENV === "test")
+if (process.env.NODE_ENV !== "test")
   server.listen(PORT, () =>
     console.log(`server running on http://localhost:${PORT}`)
   );
