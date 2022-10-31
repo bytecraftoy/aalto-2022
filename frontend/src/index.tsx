de931f1ed5606e32ea65d244bb17395d2b65b044
K00PE <105944267+KorpelainenTommi@users.noreply.github.com>
2022-10-31 12:35:19 +0200

    Added headlessui and heroicons, merged to github_ci and added redux (#6)
    * add Tailwind to frontend
    
    * convert template app css to tailwind
    
    * init github ci, probably broken
    
    * run ci against latest LTS and normal release
    
    * project testing scripts
    
    * format rest of project files with prettier
    
    * Added headlessui and heroicons, modified App.tsx and added TailwindTestModal component to test these
    
    * Component showcases headlessui disclosure
    
    * Dummy test now forces server to close before exiting
    
    * fix formatting
    
    * Prettied, also updated prettierrc.json to enfore line endings
    
    * add linting to CI
    
    * Formatted with prettier
    
    * fix .eslintrc for backend not finding tsconfig.json
    
    * Added redux but did not create a store
    
    Co-authored-by: Joonas Niemi <joonas.niemi@aalto.fi>
    Co-authored-by: Tommi Korpelainen <tommi.korpelainen@aalto.fi>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,9 +11,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/store.ts
@@ -0,0 +1,11 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+//Todo: create the store with reducers here
+
+const reducer = combineReducers({});
+
+const store = configureStore({
+  reducer,
+});
+
+export default store;
