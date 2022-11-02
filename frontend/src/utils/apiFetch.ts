37731c100245bedff46c1d41370d4d263b5fafc9
itulkki <itulkki@proton.me>
2022-11-02 19:45:29 +0200

    added functions for fetching data from backend

---
+++ frontend/src/utils/apiFetch.ts
@@ -0,0 +1,23 @@
+import {backendURL} from './backendURL';
+
+/**
+ * Fetch text data from backend using relative path. 
+ * Throws an error if the request wasn't successful.
+ */
+const apiFetch = async (path: string, init: RequestInit | undefined) => {
+    const res = await fetch(`${backendURL}${path}`, init);
+    if(!res.ok) throw new Error('request failed with status code ' + res.status);
+    return await res.text();
+};
+
+/**
+ * Fetch JSON data from backend using relative path. 
+ * Parses the data before returning.
+ * Throws an error if the request wasn't successful or if the parsing fails.
+ */
+const apiFetchJSON = async (path: string, init: RequestInit | undefined) => {
+    const text = await apiFetch(path, init);
+    return JSON.parse(text);
+};
+
+export {apiFetch, apiFetchJSON};
