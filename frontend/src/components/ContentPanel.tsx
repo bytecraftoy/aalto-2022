a11ac1a34d0dc71b556573e49a2926a3fb7bb786
K00PE <105944267+KorpelainenTommi@users.noreply.github.com>
2022-11-07 14:08:37 +0200

    Primitive category combine (#9)
    * supertest + mock test
    
    * formatted
    
    * fix to vscode / bug
    
    * app.ts start
    
    * backend no longers tries to listen any ports during tests
    
    * -
    
    * fixed the wrong camparison operator
    
    * Formatted everything >:)
    
    * added simple mirror api for connection testing
    
    * Adding panels
    
    * convert default exports to explicit
    
    * convert default exports to explicit
    
    * Modifications to app and promptiotBox
    
    * fixed broken import
    
    * More stuff, but no connection to back yet :(
    
    * Removed tsconfig from root folder because it did not actually fix what it was supposed to fix. tsconfig exists seperately for front and back
    
    * added functions for fetching data from backend
    
    * front-end now sends a request to the back-end mirror api when 'generate content' is pressed
    
    * Basic prompt combination, and also stylized the front a bit
    
    Co-authored-by: Sami Kuikka <sami.kuikka@aalto.fi>
    Co-authored-by: Ilari Tulkki <ilari.tulkki@aalto.fi>
    Co-authored-by: Joonas Niemi <joonas.niemi@aalto.fi>
    Co-authored-by: itulkki <itulkki@proton.me>
    Co-authored-by: Tommi Korpelainen <tommi.korpelainen@aalto.fi>

--- frontend/src/components/ContentPanel.tsx
+++ frontend/src/components/ContentPanel.tsx
@@ -1,13 +1,10 @@
-import { FC } from 'react';
-
-interface ContentPanelProps {
-    children: JSX.Element;
-}
-
-export const ContentPanel: FC<ContentPanelProps> = ({ children }) => {
+export const ContentPanel = (props: { children: JSX.Element }) => {
+    const { children } = props;
     return (
-        <div className="mx-auto my-auto w-full max-w-6xl rounded-xl bg-slate-700 p-10 min-h-fit">
-            {children}
+        <div className="w-full px-4 py-12">
+            <div className="mx-auto my-auto w-full max-w-6xl rounded-2xl bg-panel p-12 min-h-fit">
+                {children}
+            </div>
         </div>
     );
 };
--- frontend/src/components/PromptIOBox.tsx
+++ frontend/src/components/PromptIOBox.tsx
@@ -1,36 +1,44 @@
+import { StateHook } from '../utils/types';
+
 export const PromptIOBox = (props: {
     input: string;
-    setInput: React.Dispatch<React.SetStateAction<string>>;
+    setInput: StateHook<string>;
     output: string;
-    setOutput: React.Dispatch<React.SetStateAction<string>>;
+    setOutput: StateHook<string>;
 }) => {
     const { input, setInput, output, setOutput } = props;
+
+    const lineHeight = (text: string) => {
+        return `${Math.min(
+            20,
+            Math.max(4, text.split('\n').length) * 1.75 + 2
+        )}rem`;
+    };
+
     return (
         <div className="pt-4 w-full flex flex-row items-center justify-around">
             <div className="w-full flex flex-col items-center justify-around">
-                <h3 className="text-white font-medium text-xl pb-6">
-                    {' '}
-                    Prompts{' '}
+                <h3 className="text-textcolor font-medium text-2xl pb-6">
+                    Prompt
                 </h3>
                 <textarea
-                    id="message"
-                    rows={4}
-                    //className="block p-2.5 w-full h-1/2 text-sm text-black rounded-t-lg border-2 border-b-0 border-cyan-400 resize-none"
-                    className="w-full max-w-6xl rounded-t-xl bg-slate-500 h-20 text-white font-medium text-xl px-4 text-center"
-                    placeholder="Input box"
+                    spellCheck={'false'}
+                    className="w-full max-w-6xl rounded-t-xl bg-textfield h-36 text-textcolor font-medium text-xl px-4 py-4 outline-none outline-offset-0 focus:outline-textcolor transition-[outline-color] resize-none focus:z-10 scrollbar"
+                    placeholder="User input here"
+                    style={{ minHeight: lineHeight(input) }}
                     value={input}
-                    onInput={(e) =>
-                        setInput((e.target as HTMLTextAreaElement).value)
+                    onInput={({ target }) =>
+                        setInput((target as HTMLTextAreaElement).value)
                     }
                 />
                 <textarea
-                    id="message"
-                    rows={4}
-                    className="w-full max-w-6xl rounded-b-xl bg-slate-500 h-20 text-white font-medium text-xl px-4 text-center"
-                    placeholder="Output box"
+                    spellCheck={'false'}
+                    className="w-full max-w-6xl rounded-b-xl bg-slate-500 h-36 text-textcolor font-medium text-xl px-4 py-4 outline-none outline-offset-0 focus:outline-textcolor transition-[outline-color] resize-none focus:z-10 scrollbar"
+                    placeholder="AI generated content"
+                    style={{ minHeight: lineHeight(output) }}
                     value={output}
-                    onInput={(e) =>
-                        setOutput((e.target as HTMLTextAreaElement).value)
+                    onInput={({ target }) =>
+                        setOutput((target as HTMLTextAreaElement).value)
                     }
                 />
             </div>
