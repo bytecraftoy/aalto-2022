df7835842d98321224bb61277604cc371298d6fc
Herman Malmsten <herman.malmsten@aalto.fi>
2022-11-09 12:50:24 +0200

    initial commit, placeholder dummyrouter and dummyservice initialized

--- backend/src/routers/dummyRouter.ts
+++ backend/src/routers/dummyRouter.ts
@@ -4,13 +4,12 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     //makes sure the request is correctly formatted. WIP
-    const jsonValidation = (json: string) => true
+    const jsonValidation = (json: string) => true;
 
     if (jsonValidation(req.body)) {
-        res.send(req.body)
-    }
-    else {
-        res.status(400).json({error: 'bad request'})
+        res.send(req.body);
+    } else {
+        res.status(400).json({ error: 'bad request' });
     }
 });
 
