370d673c0d022cdb4bf5928ac14d8898ed10bdbd
K00PE <105944267+KorpelainenTommi@users.noreply.github.com>
2022-11-10 10:53:12 +0200

    Initial panel rework (#13)
    * Added utility scripts. Readme
    
    * script for checking codebase before commits
    
    * Cleaned up deploy folder because it got accidentally pushed
    
    * Updated gitignore and prettiercignore, added check and cleaning scripts
    
    * Moved logic from app to panel, added support for multiple PromptIOBoxes, rudimentary add/delete buttons for promptIOBoxes, wrote testing for Panel and related features
    
    * Added commenting
    
    * Starter button template
    
    * prettier check :)
    
    * typo
    
    Co-authored-by: Tommi Korpelainen <tommi.korpelainen@aalto.fi>
    Co-authored-by: Sami Kuikka <sami.kuikka@aalto.fi>

--- frontend/src/components/ContentPanel.tsx
+++ frontend/src/components/ContentPanel.tsx
@@ -1,9 +1,90 @@
-export const ContentPanel = (props: { children: JSX.Element }) => {
-    const { children } = props;
+import { useState, FC } from 'react';
+import { PromptCategoryBox } from './PromptCategoryBox';
+import { generateText } from '../utils/generateContent';
+import { PromptIOBox, PromptData } from './PromptIOBox';
+import { Button } from './Button';
+
+//Provide access to MasterCategory through a parent callback
+interface ContentPanelProps {
+    getMasterCategory: () => string;
+}
+
+export const ContentPanel: FC<ContentPanelProps> = () => {
+    //Component state consists of category prompt, and N multiprompts (id, input, output)
+    const [category, setCategory] = useState('');
+    const [promptBoxes, setPromptBoxes] = useState<PromptData[]>([]);
+
+    //Callbacks to add or remove PromptIOBoxes to the panels
+    const deletePromptBox = (id: string) => {
+        setPromptBoxes((prev) => prev.filter((p) => p.id !== id));
+    };
+    const addPromptBox = () => {
+        const newBox = { id: crypto.randomUUID(), input: '', output: '' };
+        setPromptBoxes((prev) => [...prev, newBox]);
+    };
+
+    //Callbacks to asynchronously fetch AI data from backend
+    const generateAll = () => promptBoxes.forEach(generateOutput);
+    const generateOutput = async (p: PromptData) => {
+        setPromptOutput(p.id, await generateText(p.input, category));
+    };
+
+    //Callback to modify the output area of a PromptIOBox by id
+    const setPromptInput = (id: string, input: string) => {
+        //Replace the box that matches id with a new object where input is changed.
+        setPromptBoxes((prev) =>
+            prev.map((o) =>
+                o.id === id ? { id: id, input: input, output: o.output } : o
+            )
+        );
+    };
+
+    //Callback to modify the input area of a PromptIOBox by id
+    const setPromptOutput = (id: string, output: string) => {
+        setPromptBoxes((prev) =>
+            prev.map((o) =>
+                o.id === id ? { id: id, input: o.input, output: output } : o
+            )
+        );
+    };
+
     return (
         <div className="w-full px-4 py-12">
             <div className="mx-auto my-auto w-full max-w-6xl rounded-2xl bg-panel p-12 min-h-fit">
-                {children}
+                <div className="flex flex-row justify-around items-center">
+                    <PromptCategoryBox
+                        category={category}
+                        setCategory={setCategory}
+                    />
+                </div>
+                <div className="flex flex-row justify-center items-center">
+                    <Button onClick={generateAll} name="Generate content" />
+                    <Button onClick={addPromptBox} name="Add box" />
+                </div>
+
+                <div className="flex flex-row flex-wrap justify-center items-center">
+                    {promptBoxes.map((p) => {
+                        return (
+                            <PromptIOBox
+                                key={p.id}
+                                input={p.input}
+                                output={p.output}
+                                setInput={(s: string) =>
+                                    setPromptInput(p.id, s)
+                                }
+                                setOutput={(s: string) =>
+                                    setPromptOutput(p.id, s)
+                                }
+                                generate={() => generateOutput(p)}
+                                deleteSelf={
+                                    promptBoxes.length > 1
+                                        ? () => deletePromptBox(p.id)
+                                        : null
+                                }
+                            />
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/PromptIOBox.tsx
+++ frontend/src/components/PromptIOBox.tsx
@@ -1,13 +1,40 @@
-import { StateHook } from '../utils/types';
+import { FC } from 'react';
 
-export const PromptIOBox = (props: {
+/**
+ * Minimum required data for representing PromptIOBox state.
+ * Should also include the attributes we care about for import/export
+ */
+export interface PromptData {
+    id: string;
     input: string;
-    setInput: StateHook<string>;
     output: string;
-    setOutput: StateHook<string>;
-}) => {
-    const { input, setInput, output, setOutput } = props;
+}
 
+/**
+ * Visible PromptIOBox state, and callback functions for
+ * performing necessary actions of the box.
+ */
+interface PromptIOBoxProps {
+    input: string;
+    output: string;
+    setInput: (s: string) => void;
+    setOutput: (s: string) => void;
+    generate: () => void;
+    deleteSelf: (() => void) | null; //null --> don't show button
+}
+/**
+ * Component containing editable textareas for Input/Output with AI
+ * generation.
+ */
+export const PromptIOBox: FC<PromptIOBoxProps> = ({
+    input,
+    output,
+    setInput,
+    setOutput,
+    generate,
+    deleteSelf,
+}) => {
+    //Calc approximately how tall a text prompt is drawn
     const lineHeight = (text: string) => {
         return `${Math.min(
             20,
@@ -16,8 +43,8 @@ export const PromptIOBox = (props: {
     };
 
     return (
-        <div className="pt-4 w-full flex flex-row items-center justify-around">
-            <div className="w-full flex flex-col items-center justify-around">
+        <div className="mt-16 pt-4 px-4 w-1/2 min-w-fit flex flex-col items-center justify-around">
+            <div className="w-full flex flex-col items-center justify-between">
                 <h3 className="text-textcolor font-medium text-2xl pb-6">
                     Prompt
                 </h3>
@@ -27,9 +54,9 @@ export const PromptIOBox = (props: {
                     placeholder="User input here"
                     style={{ minHeight: lineHeight(input) }}
                     value={input}
-                    onInput={({ target }) =>
-                        setInput((target as HTMLTextAreaElement).value)
-                    }
+                    onInput={({ target }) => {
+                        setInput((target as HTMLTextAreaElement).value);
+                    }}
                 />
                 <textarea
                     spellCheck={'false'}
@@ -37,10 +64,28 @@ export const PromptIOBox = (props: {
                     placeholder="AI generated content"
                     style={{ minHeight: lineHeight(output) }}
                     value={output}
-                    onInput={({ target }) =>
-                        setOutput((target as HTMLTextAreaElement).value)
-                    }
+                    onInput={({ target }) => {
+                        setOutput((target as HTMLTextAreaElement).value);
+                    }}
                 />
+                <div className="flex flex-row">
+                    <button
+                        className="rounded-lg bg-textfield text-textcolor h-16 min-h-fit font-medium text-xl px-4 text-center mx-6 mt-8 hover:bg-slate-500 transition-colors outline-none outline-offset-0 focus:outline-textcolor"
+                        onClick={generate}
+                    >
+                        Generate
+                    </button>
+                    {deleteSelf ? (
+                        <button
+                            className="rounded-lg bg-textfield text-textcolor h-16 min-h-fit font-medium text-xl px-4 text-center mx-6 mt-8 hover:bg-slate-500 transition-colors outline-none outline-offset-0 focus:outline-textcolor"
+                            onClick={() => deleteSelf?.()}
+                        >
+                            Delete
+                        </button>
+                    ) : (
+                        <></>
+                    )}
+                </div>
             </div>
         </div>
     );
