70cdf73fa2a6ed1402814913ed3a2c9a5bdd90b2
Tommi Korpelainen <tommi.korpelainen@aalto.fi>
2022-11-22 11:28:38 +0200

    Replaced navbar icons with heroicons

--- frontend/src/components/NavigationContainer/NavigationBar/index.tsx
+++ frontend/src/components/NavigationContainer/NavigationBar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Surface } from '../../Surface';
+import { Bars3Icon, UserCircleIcon } from '@heroicons/react/24/solid';
 
 /**
  *
@@ -9,41 +10,13 @@ import { Surface } from '../../Surface';
 
 export const NavigationBar = () => {
     return (
-        <Surface level={2} className="fixed w-full top-0 left-0 h-16 z-20 px-4">
+        <Surface level={5} className="fixed w-full top-0 left-0 h-16 z-20 px-4">
             <div className="flex flex-wrap justify-between mx-auto h-full">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 self-center"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                    />
-                </svg>
-
+                <Bars3Icon className="w-8 h-8 m-4 self-center text-neutral-30" />
                 <div className="self-center text-2xl">
                     AI-assisted game content creator
                 </div>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 self-center"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                </svg>
+                <UserCircleIcon className="w-8 h-8 m-2 self-center text-neutral-30" />
             </div>
         </Surface>
     );
