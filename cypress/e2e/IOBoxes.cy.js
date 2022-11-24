fdd1b2df2cdf54a6c4a30d935bce135657339c34
PyryManninen <pyry.manninen@gmail.com>
2022-11-24 15:26:56 +0200

    Cypress can now be ran with npm
    Added npm command for E2E testing with Cypress. Only test is for opening the page.

--- cypress/e2e/IOBoxes.cy.js
+++
@@ -1,7 +0,0 @@
-describe('add IOBox', () => {
-  it('Adds and removes IOBoxes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('Add box').click()
-    cy.contains('Prompt')
-  })
-})
