e8f210bc77e3ef14ffecbbe21ee83d1fe0e2c665
Tommi Korpelainen <tommi.korpelainen@aalto.fi>
2022-11-27 17:52:39 +0200

    Changed mirror to /api/mirror

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(bodyParser.text({ type: '*/*' }));
-app.use('/mirror', mirror);
+app.use('/api/mirror', mirror);
 app.use('/api/export/', exportRouter);
 app.use('/api/textgen', api);
 app.use(express.static('./public/'));
