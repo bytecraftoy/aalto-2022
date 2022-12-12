9310087e58650e4832f67b6e6bb1b66b7be91850
itulkki <itulkki@proton.me>
2022-12-12 15:16:19 +0200

    removed mirror router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { mirrorRouter, exportRouter, apiRouter, healthRouter } from './routers';
+import { exportRouter, apiRouter, healthRouter } from './routers';
 import cors from 'cors';
 
 const app = express();
@@ -13,7 +13,6 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(bodyParser.text({ type: '*/*' }));
-app.use('/api/mirror', mirrorRouter);
 app.use('/api/export/', exportRouter);
 app.use('/api/textgen', apiRouter);
 app.use('/health', healthRouter);
