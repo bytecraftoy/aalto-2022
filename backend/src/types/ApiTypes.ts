50ae3ae5290bb04c10dbccf6d8e1cc457cdfdc29
Joonas Niemi <joonas.niemi@aalto.fi>
2022-12-29 23:40:00 +0200

    a little bit of documentation

--- backend/src/types/ApiTypes.ts
+++ backend/src/types/ApiTypes.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 
+/*
+ * Zod validation schema
+ * Parse untrusted like so:
+ * const validated = apiRequestSchema.parse(userInputDict)
+ */
 const apiRequestSchema = z.object({
     contexts: z.array(z.string()),
     prompt: z.string().trim().min(1, 'Trimmed prompt should not be empty'),
     id: z.string().uuid(),
 });
 
+// Generate the actual type from the schema at compile time
 type ApiRequest = z.infer<typeof apiRequestSchema>;
 
 const apiResponseSchema = z.object({
