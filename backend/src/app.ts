8e07dbb41435a3f8e84752573689896a11243e94
itulkki <itulkki@proton.me>
2023-01-13 10:27:16 +0200

    CORS now only allows requests from localhost:3000 and fixed some vulnerable dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { exportRouter, apiRouter, healthRouter } from './routers';
-import cors from 'cors';
+import { cors } from './middleware/cors';
 import { requestLogger, errorLogger } from './middleware/logger';
 
 const app = express();
-app.use(cors());
+app.use(cors);
 app.use(bodyParser.text({ type: '*/*' }));
 
 // Request logger before router
---
+++ backend/src/middleware/cors.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+const cors = (
+    req: Request,
+    res: Response,
+    next: (param?: unknown) => void
+): void => {
+    //allow CORS only from localhost because that is required for development
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    //the front should be allowed to send any requests in the development mode
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    //cache the CORS information only for 1 minute so changes will take an effect quicly
+    res.header('Access-Control-Max-Age', '60');
+    next();
+};
+
+export { cors };
