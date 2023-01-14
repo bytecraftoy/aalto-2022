c9ad94d2fdf9f5709e910acd678e5db9f5ef6419
Joonas Niemi <joonas.niemi@aalto.fi>
2023-01-14 22:56:40 +0200

    initial dev docker setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import http from 'http';
 import { app } from './app';
+import { pool } from './db/pool';
+import { waitForDatabase } from './db/util';
 import { logger } from './utils/logger';
 
 if (process.env.NODE_ENV !== 'test') {
@@ -10,7 +12,13 @@ const server = http.createServer(app);
 
 if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 3030;
-    server.listen(PORT, () => logger.info('started', { port: PORT }));
+    waitForDatabase(pool)
+        .then(() => {
+            server.listen(PORT, () => logger.info('started', { port: PORT }));
+        })
+        .catch((e: unknown) => {
+            logger.error('startup_failed', { e });
+        });
 }
 
 export { server };
