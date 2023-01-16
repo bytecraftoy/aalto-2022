fb7345527abb92b880bfbf5120dae30883b6750a
jaakkojari <55512070+jaakkojari@users.noreply.github.com>
2023-01-16 17:13:51 +0200

    Addextended fab (#40)
    * Initial commit to add IOBoxButtons
    
    * IOBoxBar
    
    * Animation for buttons
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Fix Firefox bug causing input text to get wrapped
    
    * Add extended fab component and fix PromptIOBox output area label bug.
    Need to make changes to ContentPanel test
    
    * fixed the test error
    
    * disabled some un-used vars
    
    * some cypress fixes
    
    * it.only removed from cypress
    
    * deleted unecessary support file
    
    * comments to the components
    
    Co-authored-by: unknown <korpelainentommi@gmail.com>
    Co-authored-by: Tommi Korpelainen <tommi.korpelainen@aalto.fi>
    Co-authored-by: Sami Kuikka <sami.kuikka@aalto.fi>

--- frontend/src/components/ContentPanel/ContentPanelPrompts/index.tsx
+++ frontend/src/components/ContentPanel/ContentPanelPrompts/index.tsx
@@ -7,7 +7,6 @@ import { FAB } from '../../Buttons';
  * Component of Content panel.
  * Contains all the prompt boxes, i.e., contains PromoptIO boxes.
  */
-
 interface ContentPanelPromptsProps {
     promptBoxes: PromptData[];
     setPromptBoxes: React.Dispatch<React.SetStateAction<PromptData[]>>;
@@ -45,6 +44,7 @@ export const ContentPanelPrompts: React.FC<ContentPanelPromptsProps> = ({
                     <PromptIOBox
                         key={p.id}
                         id={p.id}
+                        locked={p.locked}
                         input={p.input}
                         output={p.output}
                         setInput={(s: string) => setPromptInput(p.id, s)}
