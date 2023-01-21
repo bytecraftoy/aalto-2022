43c55cc6dc9b8b84397ed49088bebf26884ab3be
Joonas Niemi <joonas.niemi@aalto.fi>
2023-01-22 00:31:25 +0200

    sql unique user name

--- backend/migrations/1674327394738_init-tables.sql
+++ backend/migrations/1674327394738_init-tables.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) UNIQUE NOT NULL,
     password_hash CHAR(60) NOT NULL,
     settings JSONB
 );
