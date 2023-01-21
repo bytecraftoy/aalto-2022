dff5099d689814779c81956fdba6dbc1cfcb64d3
Joonas Niemi <joonas.niemi@aalto.fi>
2023-01-21 21:53:04 +0200

    fix query names

--- backend/src/db/queries.ts
+++ backend/src/db/queries.ts
@@ -16,37 +16,37 @@ const executeQuery = async (text: string, values: string[]) => {
 
 const addUser = async (name: string, passwordHash: string) => {
     const text =
-        'INSERT INTO Users(Name, PasswordHash, Settings) VALUES ($1, $2)';
+        'INSERT INTO users(name, password_hash, settings) VALUES ($1, $2)';
     const values = [name, passwordHash];
     await executeQuery(text, values);
 };
 
 const deleteUser = async (id: string) => {
-    const text = 'DELETE FROM Users WHERE ID = $1';
+    const text = 'DELETE FROM users WHERE id = $1';
     const values = [id];
     await executeQuery(text, values);
 };
 
 const updatePassword = async (id: string, passwordHash: string) => {
-    const text = 'UPDATE Users SET PasswordHash = $1 WHERE ID = $2';
+    const text = 'UPDATE users SET password_hash = $1 WHERE id = $2';
     const values = [id, passwordHash];
     await executeQuery(text, values);
 };
 
 const updateUserSettings = async (id: string, settings: string) => {
-    const text = 'UPDATE Users SET Settings = $1 WHERE ID = $2';
+    const text = 'UPDATE users SET settings = $1 WHERE id = $2';
     const values = [id, settings];
     await executeQuery(text, values);
 };
 
 const addProject = async (userID: string, name: string, data: string) => {
-    const text = 'INSERT INTO Projects(UserID, Name, Data) VALUES ($1, $2, $3)';
+    const text = 'INSERT INTO projects(user_id, name, data) VALUES ($1, $2, $3)';
     const values = [userID, name, data];
     await executeQuery(text, values);
 };
 
 const deleteProject = async (id: string) => {
-    const text = 'DELETE FROM Projects WHERE ID = $1';
+    const text = 'DELETE FROM projects WHERE id = $1';
     const values = [id];
     await executeQuery(text, values);
 };
