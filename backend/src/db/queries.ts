e9f9567b68a3351a405c163784abab7fb0527765
Joonas Niemi <joonas.niemi@aalto.fi>
2023-01-23 19:30:40 +0200

    fix queries

--- backend/src/db/queries.ts
+++ backend/src/db/queries.ts
@@ -1,43 +1,45 @@
 import { pool } from './pool';
 import { logger } from '../utils/logger';
 
-const executeQuery = async (text: string, values: string[]): Promise<string[]> => {
+const executeQuery = async (text: string, values: string[]) => {
     const query = {
         text: text,
         values: values,
     };
-    await pool.connect();
-    pool.query(query, (err, res) => {
-        if (err) {
-            logger.error('db_query_fail', { error: err, query: text, values });
-            throw err;
-        }
-        logger.info(res);
-    });
-    await pool.end();
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return res.rows;
+    logger.debug('db_query_start', { query });
+    const client = await pool.connect();
+    try {
+        const res = await client.query(query);
+        client.release();
+        logger.debug('db_query_done', { res });
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return res.rows;
+    } catch (e) {
+        logger.error('db_query_fail', { error: e, query });
+    }
 };
 
 const selectProjectsbyUserID = async (userID: string) => {
-    const text = 'SELECT Name FROM Projects WHERE UserID = $1';
+    const text = 'SELECT name FROM projects WHERE user_id = $1';
     const values = [userID];
     const res = await executeQuery(text, values);
     return res;
 };
 
 const selectProjectData = async (projectID: string) => {
-    const text = 'SELECT Data FROM Projects WHERE ID = $1';
+    const text = 'SELECT data FROM projects WHERE ID = $1';
     const values = [projectID];
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const res = await executeQuery(text, values);
-    return res[0];
+    //return res[0];
 };
 
 const selectUserSettings = async (userID: string) => {
-    const text = 'SELECT Settings FROM Users WHERE UserID = $1';
+    const text = 'SELECT settings FROM users WHERE user_id = $1';
     const values = [userID];
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const res = await executeQuery(text, values);
-    return res[0];
+    //return res[0];
 };
 
 const addUser = async (name: string, passwordHash: string) => {
