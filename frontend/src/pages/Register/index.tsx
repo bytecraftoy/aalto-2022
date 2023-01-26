75cbd361bf48c2c4bb8fd80905003cb2eecd8986
Sami Kuikka <30553164+samikuikka@users.noreply.github.com>
2023-01-26 11:18:25 +0200

    Register page (#73)
    * login and register buttons to the navigation bar
    
    * start of the login page, currently without validation and not coonnected to backend
    
    * fixing input element
    
    * moved cutominput and textarea to same folder
    
    * Reusing prop information in textarea and custominput
    
    * custom hoom for input and textarea for showing errors
    
    * deleted unecessary import from textarea
    
    * moved the PromptIOBox to ContentPanelPrompts component because it's not reusable in whole application
    
    * custom hook for input elements + taking out the margin from filled button to fix bugs
    
    * loggin error message v1
    
    * closing the notification after a while from opening it
    
    * small ui changes
    
    * submit button disabled when errors on the validation
    
    * regoister screen
    
    * real hovering on the cypress tests
    
    Co-authored-by: Sami Kuikka <sami.kuikka@aalto.fi>

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { RegisterForm } from '../../components/RegisterForm';
 
 export const Register = () => {
-    return <div>Implement register screen here</div>;
+    return (
+        <div className="flex-1 flex justify-center items-center">
+            <div className="bg-surface-1 px-[3%] py-10 sm:px-[10%] rounded-xl">
+                <RegisterForm />
+            </div>
+        </div>
+    );
 };
