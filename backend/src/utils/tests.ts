0255fed71dedeffdb110237c80c38408fbc84df9
Joonas Niemi <joonas.niemi@aalto.fi>
2023-01-27 14:16:40 +0200

    reset database&migrate instead of begin/rollback

--- backend/src/utils/tests.ts
+++ backend/src/utils/tests.ts
@@ -1,18 +1,16 @@
 import { pool } from '../db/pool';
-import { waitForDatabase } from '../db/util';
+import { runMigrations } from '../db/schema';
+import { resetDatabase, waitForDatabase } from '../db/util';
 
 const timeout = 60;
 
 jest.setTimeout(timeout * 1000);
 
 beforeAll(async () => {
-    await waitForDatabase(pool, timeout);
+    await waitForDatabase(pool, false, timeout);
 });
 
 beforeEach(async () => {
-    await pool.query('BEGIN');
-});
-
-afterEach(async () => {
-    await pool.query('ROLLBACK');
+    await resetDatabase(pool);
+    await runMigrations();
 });
