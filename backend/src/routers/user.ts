fae81370d4dc78957678b7dbbb86dfe40cc9998b
itulkki <itulkki@proton.me>
2023-01-27 15:42:48 +0200

    logout now never fails

--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -91,15 +91,8 @@ userRouter.post(
     })
 );
 
-userRouter.post(
-    '/logout/',
-    expressAsyncHandler(async (req, res) => {
-        if ((await readToken(req)) === null) res.status(401).end();
-        else
-            res.cookie(tokenCookieName, '-', tokenCookieOptions)
-                .status(204)
-                .end();
-    })
+userRouter.post('/logout/', (req, res) => 
+    res.cookie(tokenCookieName, '-', tokenCookieOptions).status(204).end()
 );
 
 userRouter.get(
