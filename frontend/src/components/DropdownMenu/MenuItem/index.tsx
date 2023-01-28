bf6819850266ffd4a35b1b2469e5808f45b0139d
Sami Kuikka <sami.kuikka@aalto.fi>
2023-01-28 17:34:03 +0200

    deleted unnecessary z-index

--- frontend/src/components/DropdownMenu/MenuItem/index.tsx
+++ frontend/src/components/DropdownMenu/MenuItem/index.tsx
@@ -8,7 +8,7 @@ interface MenuItemProps {
 export const MenuItem: React.FC<MenuItemProps> = ({ onClick, message }) => {
     return (
         <div
-            className='className="block px-4 py-2 rounded bg-neutral-10 bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-8 z-[999999]'
+            className='className="block px-4 py-2 rounded bg-neutral-10 bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-8'
             onClick={onClick}
         >
             {message}
