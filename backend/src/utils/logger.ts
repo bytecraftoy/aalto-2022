cbf967f41df1314bbecc102ea820408ecb66031a
Joonas Niemi <joonas.niemi@aalto.fi>
2023-02-21 17:20:35 +0200

    Override logging level with LOG_LEVEL, use warn in testing

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,14 +1,20 @@
 import { createLogger, transports, format } from 'winston';
-import { isDevelopment } from './env';
+import { isDevelopment, isTesting } from './env';
 
 /**
  * Determines the logging level based on the current environment.
+ *  Logging levels are: error, warn, info, http, verbose, debug, silly
+ *  Use 'LOG_LEVEL' environment variable to override the default.
  *  In production we only want http (and above) level of logging, but in
  *  development all logs are enabled.
  * @return {string} The logging level to use.
  */
 const level = (): string => {
-    return isDevelopment ? 'debug' : 'http';
+    const env = process.env.LOG_LEVEL;
+    if (env) return env;
+    if (isDevelopment) return 'debug';
+    if (isTesting) return 'warn';
+    return 'http';
 };
 
 // Use pretty printing only for development environment, otherwise print on one line
