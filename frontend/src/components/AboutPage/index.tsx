4248e6405d3ce634e7904551ed62bafcd6271f4c
Tommi Korpelainen <tommi.korpelainen@aalto.fi>
2023-02-23 01:43:18 +0200

    About page components

--- frontend/src/components/AboutPage/index.tsx
+++ frontend/src/components/AboutPage/index.tsx
@@ -1,45 +1,41 @@
 import React from 'react';
-import { PromptData } from './ContentPanelPrompts/PromptIOBox';
 import { Surface } from '../Surface';
 import classNames from 'classnames';
 import { ParameterDrawer } from '../ParameterDrawer';
+import { Theme } from '../../utils/types';
+import { useAppSelector } from '../../utils/hooks';
 
+
+export interface AboutPageProps {
+    initialTheme: Theme;
+}
 
 /**
  * An about page explaining the usage of the application as well as
  * an input field for choosing a theme for the currently selected project
  *
  */
-export const AboutPage = () => {
-    const {
-        category,
-        promptBoxes,
-        loading,
-        popupOpen,
-        setCategory,
-        setPromptBoxes,
-        generateAll,
-        generateOutput,
-        setPromptOutput,
-        lockPrompt,
-        addPromptBox,
-        addPromptBoxes,
-        saveState,
-        setPopup,
-    } = usePanel(initialPrompts, initialCategory, id);
+export const AboutPage: React.FC<AboutPageProps> = ({ initialTheme }) => {
 
-    
+    useAppSelector((state) => state)
 
-    //Callback to export the category, and all inputs / outputs in json
+    //Callback to export the current project, and all its inputs / outputs in json
     const jsonExport = async () => {
-        const link = await exportJson(category, promptBoxes);
+
+        const link = await exportJson(panel);
         if (link) downloadJson(link);
     };
 
     //Callback to export outputs in excel
     //Not implemented, instead just call jsonExport
     const excelExport = async () => {
-        const link = await exportXlsx(category, promptBoxes);
+        const panel: ContentPanelData = {
+            id,
+            category,
+            prompts: promptBoxes,
+        };
+
+        const link = await exportXlsx(panel);
         if (link) downloadXlsx(link);
     };
 
@@ -103,4 +99,5 @@ export const AboutPage = () => {
             )}
         </div>
     );
+
 };
--- frontend/src/pages/About/index.tsx
+++ frontend/src/pages/About/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useLoginRedirect } from './../../utils/hooks';
+import { useAppSelector, useLoginRedirect } from './../../utils/hooks';
+import { AboutPage } from '../../components/AboutPage';
 
 export const About = () => {
     // Redirect anonymous users from this page to the login page
     useLoginRedirect();
 
+    // Get theme for the current project
+    const theme = useAppSelector((state) => state.theme.value);
+
     return (
-        <div>
-            <h1>ABOUT PAGE</h1>
+        <div className="App bg-neutral-99 flex-1 flex flex-col justify-start items-center">
+            <AboutPage
+                initialTheme={theme}
+            />
         </div>
     );
 };
