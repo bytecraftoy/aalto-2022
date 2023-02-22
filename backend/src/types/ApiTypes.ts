baafbd6c74dfa250bbd98e7335d49570b855386b
Herman <herman.malmsten@aalto.fi>
2023-02-22 15:10:36 +0200

    updated zod validation

--- backend/src/types/ApiTypes.ts
+++ backend/src/types/ApiTypes.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 export const ParametersSchema = z.object({
     creativity: z.number().min(0).max(1),
-    quality: z.number().min(0).max(9),
-    inputLength: z.number().min(0).max(10000),
-    outputLength: z.number().min(0).max(1),
+    quality: z.number().int().min(1).max(9),
+    inputLength: z.number().min(1024).max(8000),
+    outputLength: z.number().int().min(0).max(5),
 });
 
 export type Parameters = z.infer<typeof ParametersSchema>;
