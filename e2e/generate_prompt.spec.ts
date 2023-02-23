97696063e3ce317af558de7c88ac238a18db4ecb
Joonas Niemi <joonas.niemi@aalto.fi>
2023-02-23 19:12:54 +0200

    implement rest of tests in playwright

--- e2e/generate_prompt.spec.ts
+++ e2e/generate_prompt.spec.ts
@@ -57,16 +57,23 @@ test.beforeEach(async ({ page }) => {
     } else {
         await page.goto('/');
         await expect(page).toHaveURL('/login/');
-        await page.fill('span:has-text("Username") >> input', username, {
-            force: true,
-        });
-        await page.fill('span:has-text("Password") >> input', 'salasana', {
-            force: true,
-        });
-        await page.waitForTimeout(100);
-        await page.click(
-            'button:has-text("Log in") >> [data-testid="custom-button"]'
-        );
+        await page
+            .locator('label', {
+                has: page.locator('span:has-text("Username")'),
+            })
+            .locator('input')
+            .first()
+            .fill(username, { force: true });
+        await page
+            .locator('label', {
+                has: page.locator('span:has-text("Password")'),
+            })
+            .locator('input')
+            .first()
+            .fill('salasana', { force: true });
+        await page
+            .locator('[data-testid="custom-button"]:has-text("Log in")')
+            .click();
         await expect(page).toHaveURL('/');
     }
 });
@@ -82,5 +89,72 @@ test('should have no generate button without prompt input', async ({
     expect(outputText).toBe('');
 
     const generateButton = page.locator('[data-testid="iobox-Generate"]');
-    expect(generateButton).not.toBeTruthy();
+    await expect(generateButton).toHaveCount(0);
+});
+
+test('has IOBox delete button while multiple on screen', async ({ page }) => {
+    const deleteSelector = page.locator('[data-testid="iobox-Delete"]');
+    // Asserts that there is no delete button
+    await expect(deleteSelector).toHaveCount(0);
+    // Clicks the add prompt button
+    await page.click('[data-testid="fab-button"]');
+    // Asserts that there are 2 delete buttons (1 for each IOBox)
+    await expect(deleteSelector).toHaveCount(2);
+    // Hovers and clicks the first delete button
+    await page.hover('[data-testid="hover-area"]');
+    await deleteSelector.first().click();
+    // Asserts that there is no delete button (since the first IOBox was deleted)
+    await expect(deleteSelector).toHaveCount(0);
+});
+
+// This test is for checking the prompt generation feature on the application.
+// The test inputs a value for the category and prompt fields and checks that the output text area is empty.
+// Next, it clicks the "Generate" button and asserts that the output text area includes the input category and prompt.
+test('should generate a prompt and get result', async ({ page }) => {
+    await page.waitForTimeout(100);
+    await page.fill('input[placeholder*="category"]', category_input);
+    await page.fill('textarea[placeholder*="User input here"]', prompt_input);
+    await expect(page.locator(output_locator)).toHaveText('');
+    await page.hover('[data-testid="hover-area"]');
+    await page.waitForTimeout(100);
+    await page.click('button:has-text("Generate")');
+    await page.waitForTimeout(100);
+    await expect(page.locator(output_locator)).toContainText(
+        `Write a game flavor text for ${prompt_input} which is a ${category_input} in a ${theme_input} setting`
+    );
+});
+
+test('should generate all boxes with generate all button', async ({ page }) => {
+    const numExtra = 3;
+    await page.fill('input[placeholder*="category"]', category_input);
+    await expect(page.locator('[data-testid="prompt"]')).toHaveCount(1);
+    for (let i = 1; i <= numExtra; i++) {
+        // Clicks the add prompt button to add extra prompts
+        await page.click('[data-testid="fab-button"]');
+    }
+    // Confirms that the number of prompts matches the number of extra prompts added
+    await expect(page.locator('[data-testid="prompt"]')).toHaveCount(
+        1 + numExtra
+    );
+    await page
+        .locator('textarea[placeholder*="User input here"]')
+        .all()
+        .then(async (elements) => {
+            for (let i = 0; i < elements.length; i++) {
+                // Inputs a value for each prompt
+                await elements[i].fill(`${prompt_input} ${i}`);
+            }
+        });
+    // Clicks the generate all button
+    await page.waitForTimeout(100);
+    await page.click('button:has-text("Generate all")');
+    await page.waitForTimeout(100);
+    // Find all output locators and assert their text content
+    const outputAreas = page.locator(output_locator);
+    const count = await outputAreas.count();
+    for (let i = 0; i < count; i++) {
+        await expect(outputAreas.nth(i)).toContainText(
+            `Write a game flavor text for ${prompt_input} ${i} which is a ${category_input} in a ${theme_input} setting`
+        );
+    }
 });
