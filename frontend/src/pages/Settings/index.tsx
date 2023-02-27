bc774a79fd9347bef6a8286c02dae519eefe7243
unknown <korpelainentommi@gmail.com>
2023-02-27 08:20:30 +0200

    Uniform styling for login register and password change

--- frontend/src/pages/Settings/index.tsx
+++ frontend/src/pages/Settings/index.tsx
@@ -4,9 +4,7 @@ import { ChangePasswordForm } from '../../components/ChangePasswordForm';
 export const Settings = () => {
     return (
         <div className="flex-1 flex justify-center items-center">
-            <div className="bg-surface-1 px-[3%] py-10 sm:px-[10%] rounded-xl">
-                <ChangePasswordForm />
-            </div>
+            <ChangePasswordForm />
         </div>
     );
 };
