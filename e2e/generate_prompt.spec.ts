4985aa96a1bdadfb18e86c702f15fd9b7ae35e6e
unknown <korpelainentommi@gmail.com>
2023-02-27 07:58:42 +0200

    Playwright now navigates like a user would (nav drawer)

--- e2e/generate_prompt.spec.ts
+++ e2e/generate_prompt.spec.ts
@@ -109,21 +109,31 @@ test('has IOBox delete button while multiple on screen', async ({ page }) => {
 // Next, it clicks the "Generate" button and asserts that the output text area includes the input category and prompt.
 test('should generate a prompt and get result', async ({ page }) => {
     // First visit the about page to set project theme
-    await page.goto('http://localhost:3000/about');
+    // Locate left hand bar
+    await page.locator('[data-testid="navdrawer-button"]').first().click();
+    await page.locator('a[href="/about"]').first().click();
     await expect(page).toHaveURL('/about');
     const themefield = await page
         .locator('[data-testid="theme-input"]')
-        .first()
         .locator('input')
         .first();
+    await themefield.click(); // Click to properly simulate user editing (this removes focus from nav drawer)
     await themefield.fill(theme_input, { force: true });
     await expect(themefield).toHaveValue(theme_input);
-    await page.click('body'); // Get out of focus to cause autosave
-    await expect(page.locator('div:has-text("Your progress has been saved")')).toBeDefined();
-    await expect(page.locator('div:has-text("Your progress has been saved")')).toHaveCount(0); //Autosave finished
+    await themefield.blur(); // Get out of focus to cause autosave
+    await expect(
+        page.locator('div:has-text("Your progress has been saved")')
+    ).toBeDefined();
+    await expect(
+        page.locator('div:has-text("Your progress has been saved")')
+    ).toHaveCount(0); //Autosave finished
 
-    await page.goto('http://localhost:3000');
-    await expect(page).toHaveURL('/');
+    await page.locator('[data-testid="navdrawer-button"]').first().click();
+    await expect(page.getByRole('link', { name: 'Panel-1' })).toHaveCount(1);
+    await page.getByRole('link', { name: 'Panel-1' }).click();
+    await expect(page).toHaveURL(/\/panels\/.*/); // Match path, e.g. /panels/aDk4io9eRts
+    await page.click('input[placeholder*="category"]'); // Closes nav drawer
+
     await page.fill('input[placeholder*="category"]', category_input);
     await page.fill('textarea[placeholder*="User input here"]', prompt_input);
     await expect(page.locator(output_locator)).toHaveText('');
@@ -135,19 +145,32 @@ test('should generate a prompt and get result', async ({ page }) => {
 });
 
 test('should generate all boxes with generate all button', async ({ page }) => {
-        // First visit the about page to set project theme
-        await page.goto('http://localhost:3000/about');
-        await expect(page).toHaveURL('/about');
-        const themefield = await page
-            .locator('[data-testid="theme-input"]')
-            .first()
-            .locator('input')
-            .first();
-        await themefield.fill(theme_input, { force: true });
-        await expect(themefield).toHaveValue(theme_input);
-        await page.click('body'); // Get out of focus to cause autosave
-        await page.goto('http://localhost:3000');
-        await expect(page).toHaveURL('/');
+    // First visit the about page to set project theme
+    // Locate left hand bar
+    await page.locator('[data-testid="navdrawer-button"]').first().click();
+    await page.locator('a[href="/about"]').first().click();
+    await expect(page).toHaveURL('/about');
+    const themefield = await page
+        .locator('[data-testid="theme-input"]')
+        .locator('input')
+        .first();
+    await themefield.click(); // Click to properly simulate user editing (this removes focus from nav drawer)
+    await themefield.fill(theme_input, { force: true });
+    await expect(themefield).toHaveValue(theme_input);
+    await themefield.blur(); // Get out of focus to cause autosave
+    await expect(
+        page.locator('div:has-text("Your progress has been saved")')
+    ).toBeDefined();
+    await expect(
+        page.locator('div:has-text("Your progress has been saved")')
+    ).toHaveCount(0); //Autosave finished
+
+    await page.locator('[data-testid="navdrawer-button"]').first().click();
+    await expect(page.getByRole('link', { name: 'Panel-1' })).toHaveCount(1);
+    await page.getByRole('link', { name: 'Panel-1' }).click();
+    await expect(page).toHaveURL(/\/panels\/.*/); // Match path, e.g. /panels/aDk4io9eRts
+    await page.click('input[placeholder*="category"]'); // Closes nav drawer
+
     const numExtra = 3;
     await page.fill('input[placeholder*="category"]', category_input);
     await expect(page.locator('[data-testid="prompt"]')).toHaveCount(1);
