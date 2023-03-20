ce9aedbdca1c2ad05d859cda8ff6b6a5d2eaf84a
unknown <cool.seppo@hotmail.fi>
2023-03-20 21:05:35 +0200

    WIP

--- frontend/src/components/Inputs/TextArea/index.tsx
+++ frontend/src/components/Inputs/TextArea/index.tsx
@@ -54,13 +54,13 @@ export const TextArea: React.FC<TextInputProps> = ({
                 placeholder={placeholder}
                 ref={textareaRef}
                 className={classNames(
-                    `${resizable ? "resize-y" : "" }`,
-                    'form-control peer h-40 block w-full pl-4 pr-3 py-1.5 pt-3 text-base font-normal bg-clip-padding  resize-none',
+                    'form-control peer h-40 min-h-[160px] block w-full pl-4 pr-3 py-1.5 pt-3 text-base font-normal bg-clip-padding min-h-full resize-none',
                     'border-b border-onSurface focus:border-b-2 focus:border-primary focus:outline-none ',
                     'rounded-t-lg transition-colors ',
                     'bg-neutral-90  group-hover:bg-neutral-80 focus:bg-neutral-80',
                     'placeholder:text-transparent placeholder:select-none cursor-text',
-                    { 'focus:border-red border-red': showError }
+                    { 'focus:border-red border-red': showError },
+                    { 'resize-y' : resizable}
                 )}
                 value={value}
                 onInput={onInput}
