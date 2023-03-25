8d387a722b758cd01c48005d9eeb343f2e262b10
hmalmsten <98034456+hmalmsten@users.noreply.github.com>
2023-03-25 17:34:13 +0200

    Validation for high output/low input lengths in backend (#156)
    * Validation for high output/low input lengths, added tests for api/textgen
    
    * deleted cypress

--- backend/src/services/validateApiRequest.ts
+++ backend/src/services/validateApiRequest.ts
@@ -20,10 +20,19 @@ class ValidationError extends SyntaxError {
 // eslint-disable-next-line @typescript-eslint/require-await
 const validateApiRequest = async (body: string): Promise<ApiRequest> => {
     const parsed = GenerationRequestJsonSchema.parse(JSON.parse(body));
-    //currently we wan there to be exactly two contexts: theme and category
+    //currently we want there to be exactly two contexts: theme and category
     if (parsed.contexts.length !== 2)
         throw new ValidationError(
             `Found ${parsed.contexts.length} contexts instead of 2`
+        );
+
+    //throw error if output length too high with low input length
+    if (
+        parsed.parameters.outputLength === 5 &&
+        parsed.parameters.inputLength < 4000
+    )
+        throw new ValidationError(
+            `Invalid output/input length combination (${parsed.parameters.outputLength} and ${parsed.parameters.inputLength})`
         );
     return parsed as ApiRequest;
 };
