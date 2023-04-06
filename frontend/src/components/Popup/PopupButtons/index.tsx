15ad65f4da7cceb846dc4bd6c7ac0c2c411934b6
K00PE <105944267+KorpelainenTommi@users.noreply.github.com>
2023-04-06 14:58:03 +0300

    Generic popup window (#165)
    * Generic popup window
    
    * Requested changes
    
    ---------
    
    Co-authored-by: Tommi Korpelainen <tommi.korpelainen@aalto.fi>

---
+++ frontend/src/components/Popup/PopupButtons/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { PopupButton } from '..';
+import { TextButton } from '../../Buttons';
+
+export interface PopupButtonsProps {
+    close: () => void;
+    buttons: PopupButton[];
+}
+
+export const PopupButtons: React.FC<PopupButtonsProps> = ({
+    close,
+    buttons,
+}) => {
+    return (
+        <div className="p-4 w-full flex flex-row justify-between">
+            <div className="w-full" />
+            <div className="flex flex-row justify-between">
+                {buttons.map((button, idx) => {
+                    return (
+                        <TextButton
+                            key={idx}
+                            onClick={() => {
+                                // If action is defined, and returns true, do not close
+                                // Otherwise, close popup when clicked
+                                if (button.action) {
+                                    if (!button.action()) close();
+                                } else close();
+                            }}
+                            icon={button.icon}
+                            name={button.text}
+                            colorPalette="primary"
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
