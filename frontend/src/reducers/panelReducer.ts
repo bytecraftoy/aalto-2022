7d88864f92dc796ef2fac4f824038a33d65efb63
itulkki <116146033+itulkki@users.noreply.github.com>
2023-04-06 13:28:43 +0300

    No saving at log in nor at log out (#153)
    * login and logout no longer trigger saving
    
    * fixed login redirect
    
    * fixed some vulnerable dependencies
    
    * fixed panel reducer
    
    * save button now forces saving
    
    * divided the old setPanel into two different functions for differenc use cases
    
    * possible playwright fix for Chromium

--- frontend/src/reducers/panelReducer.ts
+++ frontend/src/reducers/panelReducer.ts
@@ -9,53 +9,69 @@ import { ContentPanelData, createEmptyPanel } from '../utils/types';
 // State is all the ContentPanels of the application
 interface PanelState {
     name: string;
+    needsSaving: boolean;
     value: ContentPanelData[];
 }
 
 /**
  * Initial state of the panels is 1 empty panel
  */
-const initialState: PanelState = {
-    name: 'main',
-    value: [createEmptyPanel()],
+const createIntialState = (): PanelState => {
+    return {
+        name: 'main',
+        needsSaving: false,
+        value: [createEmptyPanel()],
+    };
 };
 
 // Panel slice
 export const panelSlice = createSlice({
     name: 'panels',
-    initialState,
+    initialState: createIntialState(),
     reducers: {
-        // Sets all the panels to new ones
-        setPanels(state, action: PayloadAction<ContentPanelData[]>) {
+        // Sets all the panels to new ones that correspond to the state saved in the db
+        setSavedPanels(state, action: PayloadAction<ContentPanelData[]>) {
             state.value = action.payload;
+            state.needsSaving = false;
+        },
+        // Sets all the panels to new ones that are not saved in the db
+        setNewPanels(state, action: PayloadAction<ContentPanelData[]>) {
+            state.value = action.payload;
+            state.needsSaving = true;
         },
         // Updates a single content panel
         updatePanel(state, action: PayloadAction<ContentPanelData>) {
             state.value = state.value.map((panel) =>
                 panel.id == action.payload.id ? action.payload : panel
             );
+            state.needsSaving = true;
         },
         // Adds a new panel to the state
         addPanel(state) {
             state.value.push(createEmptyPanel());
+            state.needsSaving = false;
         },
         // Remove a specific panel
         removePanel(state, action: PayloadAction<ContentPanelData>) {
             state.value = state.value.filter(
                 (panel) => panel.id !== action.payload.id
             );
+            state.needsSaving = true;
         },
         // Empties the reducer
         clearPanels() {
-            return {
-                name: 'main',
-                value: [createEmptyPanel()],
-            };
+            return createIntialState();
         },
     },
 });
 
-export const { setPanels, updatePanel, addPanel, removePanel, clearPanels } =
-    panelSlice.actions;
+export const {
+    setSavedPanels,
+    setNewPanels,
+    updatePanel,
+    addPanel,
+    removePanel,
+    clearPanels,
+} = panelSlice.actions;
 
 export default panelSlice.reducer;
