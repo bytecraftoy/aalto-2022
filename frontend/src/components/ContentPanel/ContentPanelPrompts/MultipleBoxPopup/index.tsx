c4ae82531d852d17e92df2fd3dc3f0ae4b093be6
itulkki <itulkki@proton.me>
2023-04-07 11:48:27 +0300

    switched to a text input that can be better controlled

--- frontend/src/components/ContentPanel/ContentPanelPrompts/MultipleBoxPopup/index.tsx
+++ frontend/src/components/ContentPanel/ContentPanelPrompts/MultipleBoxPopup/index.tsx
@@ -10,6 +10,8 @@ interface MultipleBoxPopupProps {
     popupOpen: boolean;
 }
 
+const isNumeric = (text: string): boolean => /^\d*$/.test(text);
+
 export const MultipleBoxPopup: React.FC<MultipleBoxPopupProps> = ({
     addPromptBoxes,
     setPopup,
@@ -17,10 +19,13 @@ export const MultipleBoxPopup: React.FC<MultipleBoxPopupProps> = ({
 }) => {
     const [numberInput, setNumber] = useState<string>('1');
 
+    const min = 1;
+    const max = 100;
+
     // Adds multiple I/O boxes. return true to keep popup open if the input was invalid
     const addBoxes = (input: string) => {
         const n: number = Number.parseInt(input);
-        if (n >= 1 && n <= 100) {
+        if (n >= min && n <= max) {
             addPromptBoxes(n);
             return false;
         } else return true;
@@ -39,22 +44,15 @@ export const MultipleBoxPopup: React.FC<MultipleBoxPopupProps> = ({
                 <div className="px-2 py-2">Number of boxes (1-100)</div>
                 <input
                     className="px-4 py-2 rounded bg-neutral-10 bg-opacity-8"
-                    type="number"
-                    placeholder="1"
-                    min="1"
-                    max="100"
+                    type="text"
                     value={numberInput}
-                    onBeforeInput={(e) => {
-                        const d = (e.nativeEvent as KeyboardEvent).key;
-                        if (
-                            typeof d === 'string' &&
-                            Number.parseInt(d).toString() !== d
-                        )
-                            e.preventDefault();
-                    }}
                     onInput={(e) => {
-                        const t = e.target as HTMLInputElement;
-                        setNumber(t.value);
+                        const value = (e.target as HTMLInputElement).value;
+                        const data = (e.nativeEvent as InputEvent).data;
+                        const isValid =
+                            isNumeric(value) &&
+                            (data === null || isNumeric(data));
+                        if (isValid) setNumber(value);
                     }}
                 />
             </div>
