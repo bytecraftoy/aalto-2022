b9ab4564b86191ce959ea58dc00af9cb4dd2760a
Joonas Niemi <joonas.niemi@aalto.fi>
2023-04-20 19:53:30 +0300

    dev migrations + dev superadmin + field updates

---
+++ backend/dev_migrations/1682008155599_add-admin-user.sql
@@ -0,0 +1,3 @@
+-- Up Migration
+INSERT INTO users (id, name, password_hash, role)
+VALUES ('8ed32f81-31c6-488d-aa1a-416ea13e8291', 'superadmin', '$2b$10$In/OFg5exXq3cn69YwQbv.k3vwnr0k/HLYaK7Jj0bTRl1KBbawQY6', 'superadmin');
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -7,8 +7,10 @@ import { logger } from '../utils/logger';
 import migrate, { RunnerOption } from 'node-pg-migrate';
 import { Client } from 'pg';
 import { databaseConfig } from './pool';
+import { RunMigration } from 'node-pg-migrate/dist/migration';
+import { isProduction } from '../utils/env';
 
-const runMigrations = async () => {
+const migrateFolder = async (folder: string): Promise<RunMigration[]> => {
     logger.info('migrations_start');
 
     // we need to create a new client instead of using a pool because the
@@ -18,7 +20,7 @@ const runMigrations = async () => {
 
     const options: RunnerOption = {
         dbClient: client,
-        dir: 'migrations',
+        dir: folder,
         direction: 'up',
         log: (msg: string) => {
             logger.info('migrate', { msg });
@@ -31,6 +33,19 @@ const runMigrations = async () => {
     await client.end();
 
     logger.info('migrations_done', { res });
+    return res;
+};
+
+const runMigrations = async () => {
+    logger.info('migrations_start');
+    const res = await migrateFolder('migrations');
+    logger.info('migrations_done', { res });
+
+    if (!isProduction) {
+        logger.info('dev_migrations_start');
+        const dev_res = await migrateFolder('dev_migrations');
+        logger.info('dev_migrations_done', { dev_res });
+    }
 };
 
 export { runMigrations };
