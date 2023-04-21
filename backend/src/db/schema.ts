fe64c94aef22d24fe0e99de39c4a93c6442fb73e
jniemii <joonas.niemi@aalto.fi>
2023-04-21 16:49:12 +0000

    Dev sql migrations (#182)
    * dev migrations + dev superadmin + field updates
    
    * clarify
    
    * cleanup

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -7,8 +7,10 @@ import { logger } from '../utils/logger';
 import migrate, { RunnerOption } from 'node-pg-migrate';
 import { Client } from 'pg';
 import { databaseConfig } from './pool';
+import { RunMigration } from 'node-pg-migrate/dist/migration';
+import { isProduction } from '../utils/env';
 
-const runMigrations = async () => {
+const migrateFolder = async (folder: string): Promise<RunMigration[]> => {
     logger.info('migrations_start');
 
     // we need to create a new client instead of using a pool because the
@@ -18,7 +20,7 @@ const runMigrations = async () => {
 
     const options: RunnerOption = {
         dbClient: client,
-        dir: 'migrations',
+        dir: folder,
         direction: 'up',
         log: (msg: string) => {
             logger.info('migrate', { msg });
@@ -30,6 +32,17 @@ const runMigrations = async () => {
     const res = await migrate(options);
     await client.end();
 
+    logger.info('migrations_done', { res });
+    return res;
+};
+
+const runMigrations = async () => {
+    logger.info('migrations_start');
+    const res = await migrateFolder('migrations');
+
+    if (!isProduction) {
+        await migrateFolder('dev_migrations');
+    }
     logger.info('migrations_done', { res });
 };
 
